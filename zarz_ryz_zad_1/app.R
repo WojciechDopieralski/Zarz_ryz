e7aee86d6a13b42d3359d4cd09a1feaa1715c34e
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-01 17:32:15 +0100

    ...

--- zarz_ryz_zad_1/app.R
+++ zarz_ryz_zad_1/app.R
@@ -52,7 +52,7 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         textOutput("variation")
       )
    )
    )
@@ -60,15 +60,21 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  
+  
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$variation <- renderText ({
+     
+     x <- input$curr
+     y <- input$year
+     variation <- var(zarz_ryz$USD, na.rm = TRUE)
+     paste("Wartosc wariancji jest rowna: ",variation, " w roku: ", y)
+
+     
    })
+   
+   
 }
 
 # Run the application 
